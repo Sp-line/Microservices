394e908801b40b00f15af52467aea6eda7f8935d
spline <dmcemin@gmail.com>
2025-12-03 13:47:51 +0100

    add: users model

---
+++ users/core/models/user.py
@@ -0,0 +1,17 @@
+from sqlalchemy import String, Boolean
+from sqlalchemy.orm import Mapped, mapped_column
+
+from core.models import Base
+
+
+class User(Base):
+    id: Mapped[str] = mapped_column(String, primary_key=True, index=True)
+
+    username: Mapped[str] = mapped_column(String, nullable=False, unique=True)
+    avatar: Mapped[str] = mapped_column(String, nullable=True)
+
+    email: Mapped[str] = mapped_column(String, nullable=False, unique=True)
+    active: Mapped[bool] = mapped_column(Boolean, default=True)
+
+    def __repr__(self):
+        return f"<User(fief_id={self.id}, email={self.email})>"

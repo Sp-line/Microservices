1132414f828ee4664b3f9e9afe04285c72202b82
spline <dmcemin@gmail.com>
2025-12-08 17:28:00 +0100

    add: BaseValidator & validation_check decorator

---
+++ users/core/validators/base.py
@@ -0,0 +1,37 @@
+import inspect
+from enum import Enum
+from typing import Any
+
+from sqlalchemy.ext.asyncio import AsyncSession
+
+
+def validation_check(*scenarios: Enum):
+    def decorator(func):
+        func._is_validation_check = True
+        func._validation_scenarios = set(scenarios)
+        return func
+
+    return decorator
+
+
+class BaseValidator:
+    session: AsyncSession
+
+    def __init__(self, session: AsyncSession) -> None:
+        self.session = session
+
+    async def validate(self, data: dict[str, Any], scenario: Enum = None) -> None:
+        for attr_name in dir(self):
+            if attr_name.startswith("__"): continue
+
+            method = getattr(self, attr_name)
+
+            if callable(method) and getattr(method, "_is_validation_check", False):
+                method_scenarios: set[Enum] = getattr(method, "_validation_scenarios", set())
+
+                should_run = (scenario is None) or (scenario in method_scenarios)
+                if should_run:
+                    if inspect.iscoroutinefunction(method):
+                        await method(data)
+                    else:
+                        method(data)

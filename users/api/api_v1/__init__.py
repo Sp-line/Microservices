e4767b371a45036a7258c7d858ce7bfcc9d29728
spline <dmcemin@gmail.com>
2025-12-08 17:50:50 +0100

    add: register webhooks_router

--- users/api/api_v1/__init__.py
+++ users/api/api_v1/__init__.py
@@ -1,7 +1,10 @@
 from fastapi import APIRouter
 
+from api.api_v1.webhooks import webhooks_router
 from core.config import settings
 
 router = APIRouter(
     prefix=settings.api.v1.prefix,
 )
+
+router.include_router(webhooks_router)

cbf4bec575977f28dec3818b2da6f2ae2c21ae5a
spline <dmcemin@gmail.com>
2025-12-14 13:47:18 +0100

    del: static mount

--- users/main.py
+++ users/main.py
@@ -1,7 +1,4 @@
-import os
-
 import uvicorn
-from fastapi.staticfiles import StaticFiles
 
 import exceptions.handlers  # noqa
 from api import router as api_router
@@ -11,9 +8,6 @@ from core.server import main_app
 main_app.include_router(
     api_router,
 )
-
-os.makedirs("static/avatars", exist_ok=True)
-main_app.mount("/static", StaticFiles(directory="static"), name="static")
 
 if __name__ == "__main__":
     uvicorn.run(
